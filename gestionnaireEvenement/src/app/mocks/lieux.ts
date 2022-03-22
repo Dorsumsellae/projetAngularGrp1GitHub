05b4c57011c1d32dc7e6eb4ed1c9160e79928c5b
MehdiHamadi31 <mehdi.hamadi31600@gmail.com>
2022-03-22 12:11:45 +0100

    ajouts mocks

---
+++ gestionnaireEvenement/src/app/mocks/lieux.ts
@@ -0,0 +1,11 @@
+import { Lieux } from "../models/lieux";
+
+
+export const LIEUX: Lieux[] = [
+    { id_lieu: 1, nom: "Toulouse", adresse: "rue du capitole" },
+    { id_lieu: 2, nom: "Albi", adresse: "rue du developpement" },
+    { id_lieu: 3, nom: "Labege", adresse: "rue du web" },
+    { id_lieu: 4, nom: "LDNR", adresse: "rue de la reussite" },
+    { id_lieu: 5, nom: "Zenith", adresse: "rue du stress" },
+
+]
