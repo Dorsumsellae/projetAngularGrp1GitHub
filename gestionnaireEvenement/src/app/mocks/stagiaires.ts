15cfd3493b1abe18f5700e4f6589cef3a159bbf4
MehdiHamadi31 <mehdi.hamadi31600@gmail.com>
2022-03-22 12:11:45 +0100

    ajouts mocks

---
+++ gestionnaireEvenement/src/app/mocks/stagiaires.ts
@@ -0,0 +1,9 @@
+import { Stagiaire } from '../models/stagiaire';
+
+export const STAGIAIRE: Stagiaire[] = [
+  { id_stagiaire: 1, nom: 'Mouton', prenom: 'Nicolas', telephone: '1234567890', adresse: 'LDNR Labege' },
+  { id_stagiaire: 2, nom: 'Benslika', prenom: 'Dalila', telephone: '1234567890', adresse: 'LDNR Labege' },
+  { id_stagiaire: 3, nom: 'Bah', prenom: 'Djibril', telephone: '1234567890', adresse: 'LDNR Labege' },
+  { id_stagiaire: 4, nom: 'Hamadi', prenom: 'Mehdi', telephone: '1234567890', adresse: 'LDNR Labege' },
+  { id_stagiaire: 5, nom: 'Buchner', prenom: 'Nina', telephone: '1234567890', adresse: 'LDNR Labege' },
+];
