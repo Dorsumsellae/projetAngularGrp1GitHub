a1532683941961731f666bf63db349c640cb3385
Nicolas MOUTON--BESSON <nicolas.moutonbesson@gmail.com>
2022-03-22 18:00:13 +0100

    start vue eventAfficher

--- gestionnaireEvenement/src/app/app.module.ts
+++ gestionnaireEvenement/src/app/app.module.ts
@@ -9,13 +9,21 @@ import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
 import { ContactComponent } from './components/contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StagiaireComponent } from './components/stagiaire/stagiaire.component';
 import { StagiaireDetailsComponent } from './components/stagiaire-details/stagiaire-details.component';
+import { EvenementAfficherComponent } from './components/evenement-afficher/evenement-afficher.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +31,7 @@ import { StagiaireDetailsComponent } from './components/stagiaire-details/stagia
     StagiaireComponent,
     ContactComponent,
     StagiaireDetailsComponent,
+    EvenementAfficherComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +40,11 @@ import { StagiaireDetailsComponent } from './components/stagiaire-details/stagia
     GoogleMapsModule,
     MatCardModule,
     MatFormFieldModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
     FormsModule,
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
--- gestionnaireEvenement/src/app/components/evenement-afficher/evenement-afficher.component.ts
+++ gestionnaireEvenement/src/app/components/evenement-afficher/evenement-afficher.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { EVENEMENTS } from 'src/app/mocks/evenement';
+import { Evenement } from 'src/app/models/evenement';
 
 @Component({
   selector: 'app-evenement-afficher',
   templateUrl: './evenement-afficher.component.html',
-  styleUrls: ['./evenement-afficher.component.scss']
+  styleUrls: ['./evenement-afficher.component.scss'],
 })
 export class EvenementAfficherComponent implements OnInit {
+  eventsFuture: Evenement[] = EVENEMENTS;
+  eventsPast: Evenement[] = EVENEMENTS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
