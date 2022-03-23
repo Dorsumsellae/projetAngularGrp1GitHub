77a8a21b1efe01d84c4ced0d2f9f24f84b46274e
Nicolas MOUTON--BESSON <nicolas.moutonbesson@gmail.com>
2022-03-23 07:37:16 +0100

    idStagiaireToStagiare

--- gestionnaireEvenement/src/app/services/invite-evenement.service.ts
+++ gestionnaireEvenement/src/app/services/invite-evenement.service.ts
@@ -10,10 +10,36 @@ export class InviteEvenementService {
   invites_evenement!: Invite_evennement[];
   invites_evenementUrl = 'http://[::1]:3000/invites-evennements';
 
+  /**
+   * Fait appelle a l'api pour obtenir Observable<Invite_evennement[]>
+   * @returns Observable<Invite_evennement[]>
+   */
   getInviteEvenement(): Observable<Invite_evennement[]> {
     return this.http.get<Invite_evennement[]>(this.invites_evenementUrl);
   }
 
+  /**
+   * Filtre les "Invite_evennement " pour trouver ceux qui concerne l'évènement (id_evenement) et renvoie un tableau des identifiants des stagiaires invités
+   * @param id_evenement
+   * @param invites_evenement
+   * @returns tableau des identifiants des stagiaires invités
+   */
+  getGuestsOfEvent(
+    id_evenement: number,
+    invites_evenement: Invite_evennement[]
+  ): number[] {
+    let guestsOfEvent: number[];
+    guestsOfEvent = invites_evenement
+      .filter((guestEvent) => guestEvent.id_evenement == id_evenement)
+      .map((guestEvent) => guestEvent.id_stagiaire);
+    return guestsOfEvent;
+  }
+
+  /**
+   * Post un invité_évènement
+   * @param invite_evenement
+   * @returns
+   */
   ajoutInviteEvenement(invite_evenement: Invite_evennement): Observable<any> {
     return this.http.post(this.invites_evenementUrl, invite_evenement);
   }
---
+++ gestionnaireEvenement/src/app/services/stagiaire.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Stagiaire } from '../models/stagiaire';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StagiaireService {
+  idStagiaireToStagiare(
+    id_stagiaire: number,
+    stagiaires: Stagiaire[]
+  ): Stagiaire {
+    let stagiaire: Stagiaire;
+    let result = stagiaires.find(
+      (stag: Stagiaire) => stag.id_stagiaire == id_stagiaire
+    );
+    if (result == undefined) {
+      stagiaire = {
+        id_stagiaire: -1,
+        nom: '0',
+        prenom: '0',
+        telephone: '0',
+        adresse: '0',
+      };
+    } else {
+      stagiaire = result;
+    }
+    return stagiaire;
+  }
+
+  constructor() {}
+}
