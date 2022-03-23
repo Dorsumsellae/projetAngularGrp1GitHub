13251aba7897a615aa2e57cae52d665dad9d25e7
Nicolas MOUTON--BESSON <nicolas.moutonbesson@gmail.com>
2022-03-23 21:08:18 +0100

    correction formulaires

--- gestionnaireEvenement/src/app/components/contact/contact.component.ts
+++ gestionnaireEvenement/src/app/components/contact/contact.component.ts
@@ -4,19 +4,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
   form = new FormGroup({
     nom: new FormControl('', Validators.required),
     prenom: new FormControl('', Validators.required),
     telephone: new FormControl('', Validators.required),
-    mail: new FormControl('', Validators.email),
-  })
-  constructor() { }
+    mail: new FormControl('', [Validators.email, Validators.required]),
+  });
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
