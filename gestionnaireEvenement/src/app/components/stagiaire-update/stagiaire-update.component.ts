ca28b6945b07adb938bc73866631b4cdce929e85
MehdiHamadi31 <mehdi.hamadi31600@gmail.com>
2022-03-24 22:07:01 +0100

    stagiaire update

--- gestionnaireEvenement/src/app/components/stagiaire-update/stagiaire-update.component.ts
+++ gestionnaireEvenement/src/app/components/stagiaire-update/stagiaire-update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Stagiaire } from 'src/app/models/stagiaire';
 
 @Component({
   selector: 'app-stagiaire-update',
@@ -6,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stagiaire-update.component.scss']
 })
 export class StagiaireUpdateComponent implements OnInit {
+  formupdStag = new FormGroup({
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl(''),
+    telephone: new FormControl(''),
+    adresse: new FormControl(''),
+  });
+  stags: any;
 
+  updStag() {
+    if (!this.formupdStag.invalid) {
+      console.log(this.formupdStag.value);
+      let stag = this.formValueToStag();
+      console.log(stag);
+      this.stags.updStagiaire(stag).subscribe();
+    }
+  }
+  formValueToStag(): Stagiaire {
+    return {
+      nom: this.formupdStag.value.nom,
+      prenom: this.formupdStag.value.prenom,
+      telephone: this.formupdStag.value.telephone,
+      adresse: this.formupdStag.value.adresse,
+    } as Stagiaire;
+  }
   constructor() { }
 
   ngOnInit(): void {
