37c907fb49b8a13977ebaf0b02c5d4c7a3e8190b
Dalila <dalila@gmail.com>
2022-03-26 08:25:29 +0100

    Composant home

--- gestionnaireEvenement/src/app/components/home/home.component.ts
+++ gestionnaireEvenement/src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { EvenementService } from 'src/app/services/evenement.service';
+import { EvenementAfficherComponent } from '../evenement-afficher/evenement-afficher.component';
+import { Evenement } from 'src/app/models/evenement';
+import { LieuService } from 'src/app/services/lieu.service';
+import { LieuxComponent } from '../lieux/lieux.component';
+import { Lieux } from 'src/app/models/lieux';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  eventsFuture!: Evenement[];
+  eventsPast!: Evenement[];
+  lieux!: Lieux[];
+
+  updateEvents() {
+    this.eventsFuture = [];
+    this.eventsPast = [];
+
+    this.eventService.getEvenement().subscribe((res) => {
+      res.forEach((evenement) => {
+        if (new Date(evenement.Jour) > new Date(Date.now())) {
+          this.eventsFuture.push(evenement);
+        } else {
+          this.eventsPast.push(evenement);
+        }
+      });
+      this.ls.getLieux().subscribe((res: Lieux[]) => {
+        this.lieux = res;
+      });
+    });
+  }
+
+  constructor(private eventService: EvenementService,
+    public ls: LieuService,) { }
 
   ngOnInit(): void {
   }
+
 
 }
